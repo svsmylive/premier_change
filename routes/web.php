81ec1973abb4c1211e3702a9110708fb1cccf290
Leonid <chakalidi@greensight.ru>
2025-01-30 21:51:48 +0300

    default view

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\CurrenciesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/currencies:calculate', [CurrenciesController::class, 'calculate'])->name('currencies.calculate');
+
+Route::get('/', function () {
+    return view('welcome');
+});
