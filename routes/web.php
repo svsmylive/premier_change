adce1e1a8e8cb529c8087d8719bac68b0c58e3c5
Leonid <chakalidi@greensight.ru>
2025-02-01 18:44:48 +0300

    front

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use App\Http\Controllers\CurrenciesController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 Route::get('/currencies:calculate', [CurrenciesController::class, 'calculate'])->name('currencies.calculate');
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/sitemap.xml', function () {
+    return Sitemap::create()
+        ->add(Url::create('/'))
+        ->toResponse(request());
 });
