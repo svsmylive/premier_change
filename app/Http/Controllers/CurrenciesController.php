b0d3799a5a53016b16cb0c6510c1b482b4265fe5
Leonid <chakalidi@greensight.ru>
2025-05-21 00:16:13 +0300

    Rapira api

--- app/Http/Controllers/CurrenciesController.php
+++ app/Http/Controllers/CurrenciesController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Str;
 
 class CurrenciesController
 {
+    //Гарантекс
     /*public function calculate(SearchCurrenciesRequest $request): array
     {
         $data = $request->validationData();
@@ -114,7 +115,8 @@ class CurrenciesController
         ];
     }*/
 
-    public function calculate(SearchCurrenciesRequest $request): array
+    //Abcex
+    /*public function calculate(SearchCurrenciesRequest $request): array
     {
         $data = $request->validated();
         $currencyFrom = $data['currency_from'];
@@ -169,6 +171,111 @@ class CurrenciesController
         $averagePrice = $askPrice - $askPrice * 0.003; //наша наценка
 
         $total = round($averagePrice, 2) * $clientSum;
+
+        return [
+            'price' => number_format($averagePrice, 2, '.', ' '),
+            'total' => number_format($total, 2, '.', ' '),
+            'currency_from' => $currencyFrom,
+            'currency_to' => $currencyTo,
+        ];
+    }*/
+
+    //Rapira
+    public function calculate(SearchCurrenciesRequest $request): array
+    {
+        $data = $request->validationData();
+        $currencyFrom = $data['currency_from'];
+        $currencyTo = $data['currency_to'];
+        $clientSum = (float)$data['sum'];
+
+        $query = Str::upper($currencyFrom) . '/' . Str::upper($currencyTo);
+
+        $cacheKey = 'currencies_' . $query . (int)$clientSum;
+
+        try {
+            $response = Cache::get('', function () use ($query, $cacheKey) {
+                $request = Http::baseUrl('https://api.rapira.net');
+                $request->timeout(30);
+                $request->connectTimeout(30);
+
+                $response = $request->get('/market/exchange-plate-mini?symbol=' . $query);
+
+                $json = $response->json();
+
+                Cache::put($cacheKey, $json, CarbonInterval::minutes(15));
+
+                return $json;
+            });
+        } catch (\Throwable $e) {
+            return [
+                'success' => false,
+                'message' => $e->getMessage(),
+            ];
+        }
+
+        $asks = $response['ask']; // ордера покупки
+        $bids = $response['bid']; // ордера продажи
+
+        $averagePrice = 0.0;
+        $remainder = 0.0;
+        $averageData = [];
+
+        foreach ($bids['items'] as $bid) {
+            $volumeCurrency = (float)$bid['amount'];
+            $bidPrice = (float)$bid['price'];
+
+            if ($clientSum <= $volumeCurrency && $remainder == 0) {
+                $averagePrice = $bidPrice;
+
+                break;
+            } else {
+                if ($remainder == 0) {
+                    $remainder = $clientSum - $volumeCurrency;
+
+                    $averageData[] = [
+                        'price' => $bidPrice,
+                        'need' => $volumeCurrency,
+                    ];
+
+                    continue;
+                }
+
+                if ($remainder <= $volumeCurrency) {
+                    $averageData[] = [
+                        'price' => $bidPrice,
+                        'need' => $remainder,
+                    ];
+
+                    break;
+                }
+
+                $averageData[] = [
+                    'price' => $bidPrice,
+                    'need' => $volumeCurrency,
+                ];
+
+                $remainder -= $volumeCurrency;
+            }
+        }
+
+        $total = 0.0;
+
+        if (!empty($averageData)) {
+            foreach ($averageData as $data) {
+                $total = $total + $data['price'] * $data['need'];
+            }
+
+            $averagePrice = $total / $clientSum;
+            $averagePrice = $averagePrice - $averagePrice * 0.003; //наша наценка
+
+            $total = round($averagePrice, 2) * $clientSum;
+        }
+
+        if ($total == 0) {
+            $averagePrice = $averagePrice - $averagePrice * 0.003; //наша наценка
+
+            $total = round($averagePrice, 2) * $clientSum;
+        }
 
         return [
             'price' => number_format($averagePrice, 2, '.', ' '),
