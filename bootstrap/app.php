16a711e5e6d619f78962ac5907af902d0cfa2546
Leonid <chakalidi@greensight.ru>
2025-11-08 17:04:55 +0300

    механизм наценки телеграм

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(append: [
+            // Игнорируем CSRF для Telegram webhook
+            VerifyCsrfToken::except([
+                'telegram/webhook/*',
+            ]),
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
